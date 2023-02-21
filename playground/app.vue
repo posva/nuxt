e9d93c6a1cadaf1461cd1374443b41d7e7d7aa4a
Eduardo San Martin Morote <posva13@gmail.com>
2023-02-21 16:02:10 +0100

    chore: add pages to playground to test

--- playground/app.vue
+++ playground/app.vue
@@ -2,12 +2,8 @@
 </script>
 
 <template>
-  <!-- Edit this file to play around with Nuxt but never commit changes! -->
-  <div>
-    Nuxt 3 Playground
-  </div>
+  <NuxtPage />
 </template>
 
 <style scoped>
-
 </style>
